fb07e64eb02ff7943c3049f459ecb8463c10a797
muschellij2 <muschellij2@gmail.com>
2019-03-31 23:25:30 -0400

    fixing devices

--- R/list_input_devices.R
+++ R/list_input_devices.R
@@ -10,6 +10,7 @@
 #'
 #' @examples
 #' list_input_devices()
+#' list_formats()
 list_input_devices = function(
   device = guess_recording_device(),
   args = c("-hide_banner", "-f", device, "-list_devices", "true")
@@ -28,4 +29,31 @@ list_input_devices = function(
   string = res$read_all_error()
   string = string[ grepl("device", string)]
   string
+}
+
+#' @export
+#' @rdname list_input_devices
+list_formats = function() {
+  args = "-formats"
+  res = processx::process$new(command = "ffmpeg",
+                              args = args,
+                              stderr = "|",
+                              stdout = "|")
+
+  devs = paste(res$read_output_lines(), collapse = "\n")
+  devs = strsplit(devs, "\n")[[1]]
+  devs = trimws(devs)
+  breaker = grep("--", devs)
+  if (length(breaker) == 1) {
+    devs = devs[(breaker + 1):length(devs)]
+  }
+  devs = gsub("\\s+", " ", devs)
+  devs = strsplit(devs, " ")
+  devs = sapply(devs, function(x) {
+    c(x[1:2], paste(x[3:length(x)], collapse = " "))
+  })
+  devs = t(devs)
+  colnames(devs) = c("capabilities", "device", "description")
+  devs = as.data.frame(devs, stringsAsFactors = FALSE)
+  return(devs)
 }
